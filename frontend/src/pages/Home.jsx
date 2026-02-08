6cf9b554e1ac62576f751c45ef1a4e450701b81a
HARISH-YADAV483 <harish4nitkkr@gmail.com>
2026-02-09 01:20:12 +0530

    Update footer to include full name of developer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -60,7 +60,7 @@ export default function Home() {
 
       {/* FOOTER */}
       <footer className="footer">
-        👨‍💻 Developed by <span>Harish </span>
+        👨‍💻 Developed by <span>Harish Puhaniya </span>
       </footer>
 
       {/* STYLES */}
